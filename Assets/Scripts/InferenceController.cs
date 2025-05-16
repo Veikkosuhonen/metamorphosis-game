13dd24627a8997173babcd9bae6ccc176603e44e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-16 20:33:13 +0300

    add high fidelity hands

--- Assets/Scripts/InferenceController.cs
+++ Assets/Scripts/InferenceController.cs
@@ -48,23 +48,23 @@ public class InferenceController : MonoBehaviour
 
     //just to help with dev since the list given by pose does not have any labels, just a list of floats
     public string[] indexToBodyPartName = new string[] {
-            "nose",
-            "leftEye",
-            "rightEye",
-            "leftEar",
-            "rightEar",
-            "leftShoulder",
-            "rightShoulder",
-            "leftElbow",
-            "rightElbow",
-            "leftWrist",
-            "rightWrist",
-            "leftHip",
-            "rightHip",
-            "leftKnee",
-            "rightKnee",
-            "leftAnkle",
-            "rightAnkle"
+            "nose", //0
+            "leftEye", //1
+            "rightEye", //2
+            "leftEar", //3
+            "rightEar", //4
+            "leftShoulder", //5
+            "rightShoulder", //6
+            "leftElbow", //7
+            "rightElbow", //8
+            "leftWrist", //9
+            "rightWrist", //10
+            "leftHip", //11
+            "rightHip", //12
+            "leftKnee", //13
+            "rightKnee", //14
+            "leftAnkle", //15
+            "rightAnkle" //16
         };
     public HumanPose2D[] humanPoses;
 
--- Assets/Scripts/PlayerModelManager.cs
+++ Assets/Scripts/PlayerModelManager.cs
@@ -1,5 +1,7 @@
+using CJM.HumanPose2DToolkit;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class PlayerModelManager : MonoBehaviour
@@ -11,9 +13,7 @@ public class PlayerModelManager : MonoBehaviour
         [Tooltip("GameObject to control")]
         public GameObject gameObject;
 
-        [Tooltip("Index of the part in the humanpose2d from where position is read")]
-        //index of the part in the HumanPose2D
-        public int index;
+      
     }
     //This reference allows to read the HumanPose2D as well as many other things =)
     public InferenceController inferenceController;
@@ -26,28 +26,15 @@ public class PlayerModelManager : MonoBehaviour
     public float movementMultiplier = 1.0f;
 
 
+
+
     public CharacterPart head;
 
-    //upper body
-    public CharacterPart wrist_l;
-    public CharacterPart elbow_l;
-    public CharacterPart shoulder_l;
-
-    public CharacterPart wrist_r;
-    public CharacterPart elbow_r;
-    public CharacterPart shoulder_r;
+    //keep at same size as the inferencecontrollers 
+    public CharacterPart[] CharacterParts;
 
 
-    //lower body
-    public CharacterPart foot_l;
-    public CharacterPart knee_l;
-    public CharacterPart hip_l;
-
-    public CharacterPart foot_r;
-    public CharacterPart knee_r;
-    public CharacterPart hip_r;
-
-    
+   
 
 
     // Start is called before the first frame update
@@ -59,12 +46,27 @@ public class PlayerModelManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float xp = inferenceController.humanPoses[0].bodyParts[0].coordinates[0];
-        float yp = inferenceController.humanPoses[0].bodyParts[0].coordinates[1];
+        for(int i = 0; i < inferenceController.humanPoses[0].bodyParts.Length; i++) { 
+            BodyPart2D bodyPart = inferenceController.humanPoses[0].bodyParts[i];
+            float xp = bodyPart.coordinates[0];
+            float yp = bodyPart.coordinates[1];
 
-        float x = (xp - screenW / 2) / screenW * movementMultiplier;
-        float y = (yp - screenH / 2) / screenH * movementMultiplier;
+            float x = (xp - screenW / 2) / screenW * movementMultiplier;
+            float y = (yp - screenH / 2) / screenH * movementMultiplier;
 
-        head.gameObject.transform.position = new Vector3(x, y, head.gameObject.transform.position.z);
+            if(i > CharacterParts.Length - 1)
+            {
+                continue;
+            }
+
+            if (CharacterParts[i].gameObject == null)
+            {
+                continue;
+            }
+
+            CharacterParts[i].gameObject.transform.position = new Vector3(x, y, CharacterParts[i].gameObject.transform.position.z);
+        }
+       
+     
     }
 }
