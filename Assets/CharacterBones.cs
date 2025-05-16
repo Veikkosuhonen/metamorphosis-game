a9a7cd0a1de36d2aca4b041b8b353475d3202cd1
Veikkosuhonen <vesuvesuh@gmail.com>
2025-05-16 18:29:03 +0300

    asd

---
+++ Assets/CharacterBones.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using CJM.HumanPose2DToolkit;
+using UnityEngine;
+
+public class CharacterBones : MonoBehaviour
+{
+    public HumanPose2D pose;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
