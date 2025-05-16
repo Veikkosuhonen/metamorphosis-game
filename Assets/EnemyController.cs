2196c26e85706f53aad13fc65b2eb33f85d2e22d
Veikkosuhonen <vesuvesuh@gmail.com>
2025-05-16 21:35:12 +0300

    enemiees

--- Assets/EnemyController.cs
+++ Assets/EnemyController.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+
+    public float speed = 10.0f;
+    public float upDownMovement = 3.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,7 +16,7 @@ public class EnemyController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float speed = 2.0f;
         transform.position -= Vector3.right * speed * Time.deltaTime;
+        transform.position = new Vector3(transform.position.x, Mathf.Sin(Time.time + transform.position.x) * upDownMovement, transform.position.z);
     }
 }
