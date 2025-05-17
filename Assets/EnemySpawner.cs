c269be00734e824fc0d8c924d912681741e4fd1e
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-17 15:10:07 +0300

    spawn enemies

---
+++ Assets/EnemySpawner.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float spawnedEnemySpeed;
+    public GameObject enemyPrefab;
+
+    public float lastSpawn = 0.0f;
+    public float spawnRate = 5.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        //spawn enemy when spawnRate is reached
+        if (Time.time > lastSpawn + spawnRate)
+        {
+            SpawnEnemy();
+            lastSpawn = Time.time;
+        }
+    }
+
+    void SpawnEnemy()
+    {
+        GameObject enemy = Instantiate(enemyPrefab, transform.position, Quaternion.identity);
+        enemy.GetComponent<EnemyController>().speed = spawnedEnemySpeed;
+    }
+}
