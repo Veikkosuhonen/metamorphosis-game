1e992ec21a5ce5066bfab77ed16142ae8f82a1bb
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-17 15:50:27 +0300

    add xp increase

--- Assets/PlayerXP.cs
+++ Assets/PlayerXP.cs
@@ -4,13 +4,12 @@ using UnityEngine;
 
 public class PlayerXP : MonoBehaviour
 {
-    
+    public int playerXP = 0; // Player's XP
 
     public void EnemyDefeated(EnemyController enemy)
     {
         // Add XP to the player
         Debug.Log("Enemy defeated! Gained XP.");
-        // You can add your XP logic here
-        // For example, increase player's XP by a certain amount
+        playerXP += 1;
     }
 }
