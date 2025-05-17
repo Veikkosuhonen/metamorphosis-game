cddb19c61856e9145cede8bef952e8fca8a75291
MatiasSinisalo <matias.aleksanteri@hotmail.fi>
2025-05-17 15:54:37 +0300

    increase xp and display it

--- Assets/PlayerXP.cs
+++ Assets/PlayerXP.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class PlayerXP : MonoBehaviour
 {
     public int playerXP = 0; // Player's XP
+    public TextMeshProUGUI xpDisplay;
 
     public void EnemyDefeated(EnemyController enemy)
     {
         // Add XP to the player
         Debug.Log("Enemy defeated! Gained XP.");
-        playerXP += 1;
+        changeXp(1);
+
+    }
+
+    public void changeXp(int amount)
+    {
+        playerXP += amount;
+        xpDisplay.text = "XP: " + playerXP.ToString();
     }
 }
